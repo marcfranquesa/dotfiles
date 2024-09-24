dc6407261e68e901ef67cab7f183ce7cba69ffcc
Marc Franquesa <git@marcfranquesa.com>
2024-09-24 17:11:15 +0200

    [misc] small changes * add a couple nvim maps * remove xdg open alias

--- .config/nvim/lua/config/maps.lua
+++ .config/nvim/lua/config/maps.lua
@@ -9,7 +9,13 @@ end
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-map("i", "jj", "<Esc>")
+map("n", ";", ":", { silent = false }) -- semicolon to enter command mode
+
+map("n", "<C-d>", "<C-d>zz") -- center when jumping around
+map("n", "<C-u>", "<C-u>zz") -- center when jumping around
+
+map("n", "<leader>j", "<cmd>bnext<cr>") -- move to right buffer
+map("n", "<leader>k", "<cmd>bprevious<cr>") -- move to left buffer
 
 map("n", "<leader>3", ":w! | :AsyncRun vim_compile '<c-r>%'<CR>") -- compile documents
 map("n", "<leader>4", ":!vim_output <c-r>%<CR><CR>") -- open compiled documents
