4c3a10774852c01c034326552d0b1fa130fc3578
Marc Franquesa Monés <git@marcfranquesa.com>
2026-05-31 16:32:43 -0400

    [tex,zathura] fix synctex integration

--- .config/nvim/after/ftplugin/tex.lua
+++ .config/nvim/after/ftplugin/tex.lua
@@ -3,11 +3,43 @@ vim.wo.spell = true
 local function synctex_jump()
     local line = vim.fn.line(".")
     local column = vim.fn.col(".")
-    local file = vim.fn.expand("%")
-    local cmd = ":silent !open-compiled " .. file .. " " .. line .. " " .. column
-    vim.cmd(cmd)
+    local file = vim.fn.expand("%:p")
+    local server = vim.v.servername
+
+    if server == "" then
+        server = vim.fn.serverstart()
+    end
+
+    local job = vim.fn.jobstart({ "open-compiled", file, tostring(line), tostring(column) }, {
+        detach = true,
+        env = { NVIM_SERVER = server },
+    })
+
+    if job <= 0 then
+        vim.notify("Failed to start SyncTeX forward search", vim.log.levels.ERROR)
+    end
 end
 
-vim.api.nvim_create_user_command("SynctexJump", synctex_jump, {})
+local function open_compiled()
+    local file = vim.fn.expand("%:p")
+    local server = vim.v.servername
 
-vim.api.nvim_set_keymap("n", "<cr>", ":SynctexJump<cr>", { noremap = true, silent = true })
+    if server == "" then
+        server = vim.fn.serverstart()
+    end
+
+    local job = vim.fn.jobstart({ "open-compiled", file }, {
+        detach = true,
+        env = { NVIM_SERVER = server },
+    })
+
+    if job <= 0 then
+        vim.notify("Failed to open compiled PDF", vim.log.levels.ERROR)
+    end
+end
+
+vim.api.nvim_buf_create_user_command(0, "SynctexJump", synctex_jump, {})
+vim.api.nvim_buf_create_user_command(0, "OpenCompiled", open_compiled, {})
+
+vim.keymap.set("n", "<cr>", synctex_jump, { buffer = true, silent = true, desc = "SyncTeX forward search" })
+vim.keymap.set("n", "<leader>o", open_compiled, { buffer = true, silent = true, desc = "Open compiled PDF" })
